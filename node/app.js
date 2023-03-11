f144e886686703274a450fbdb0907cb68fa791b3
johnjainc <116500864+johnjainc@users.noreply.github.com>
2023-03-11 08:45:42 +0530

    update-0
    port

--- node/app.js
+++ node/app.js
@@ -10,7 +10,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => console.log("connected"))
   .catch(err => console.log(err));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3030);
 
 app.use(express.urlencoded({ extended: true }));
 app.use( express.static( "public" ) );
